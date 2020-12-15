923999d0e87a7487339468a15367e0ab6d1f6a47
bibyzan <benrasmonkey@gmail.com>
2020-12-14 20:16:26 -0500

    added files

---
+++ MobileApp/src/reducers/index.js
@@ -0,0 +1,31 @@
+import { combineReducers } from 'redux';
+import auth from './auth';
+import user from './user';
+import likes from './likes';
+import posts from './posts';
+import flagged from './flagged';
+import detail from './detail';
+import comments from './comments';
+import replies from './replies';
+import timeline from './timeline';
+import profile from './profile';
+import galleries from './gallieries';
+import stats from './stats';
+
+const reducers = () =>
+  combineReducers({
+    auth,
+    user,
+    likes,
+    posts,
+    flagged,
+    detail,
+    comments,
+    replies,
+    timeline,
+    profile,
+    galleries,
+    stats,
+  });
+
+export default reducers;
